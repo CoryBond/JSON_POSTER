68a1a79a19d1256fc41a6aa9ad1c3718339e8f55
Cory Bond <cb.cobond@gmail.com>
2016-05-24 22:02:09 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,18 +11,18 @@ with open('resume_base64.bin', 'r') as myfile:
 resume_json = {
         'first_name': 'Cory',
         'last_name' : 'Bond',
-        'email' : 'cb.cobond@gmail.com',
+        'email' : '[YOUR_EMAIL_HERE',
         'position_id' : 'STE',
         'explanation' : 'Simple python script I made today. Uses urllib to make a post request to the url endpoint with my data formatted as a json object. I used the website: http://www.motobit.com/util/base64-decoder-encoder.asp to encode my pdf into a file with my base64. The json is encoded in utf-8.',
         'project' : ['https://github.com/CoryBond/CBTEN'],
-        'source' : 'I found Perka first on AngelList. I did more research about your company on your own website and it looks like a fantastic place to grow a career!',
+        'source' : 'I found [COMPANY_NAME_HERE] first on AngelList. I did more research about your company on your own website and it looks like a fantastic place to grow a career!',
         'resume' : base64_Resume
     }
 #urllib.urlopen("http://abc.com/api/posts/create",urllib.urlencode(resume_json))
 
 #print(base64_Resume)
 
-req = urllib.request.Request('https://api.perka.com/1/communication/job/apply')
+req = urllib.request.Request('[JSON_RECIEVER_URL]')
 req.add_header('Content-Type', 'application/json')
 
 #data = urllib.parse.quote_plus(json.dumps(resume_json))
